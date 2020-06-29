f410dabe80fecdcc365cbf61389901abefaa50d2
Diotr <35659983+Diotr@users.noreply.github.com>
2020-06-29 20:37:50 +0100

    components created

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../css/Header.css';
+
+const Header = () =>{
+    return(
+        <div className = "header">
+            <h1>header</h1>
+            <p>to jest header</p>
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/Technologies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/Technologies.css';
+
+const Technologies = () =>{
+    return(
+        <div className = "technologies">
+            <h1>Technologies</h1>
+        </div>
+    )
+}
+export default Technologies;
