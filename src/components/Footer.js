9dcfe4a85a51098378fec3ea7a9e8c537aa3dd7f
Diotr <35659983+Diotr@users.noreply.github.com>
2020-08-06 20:56:04 +0100

    technologies icons added and other small improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../css/Footer.css';
 const Footer = () =>{
     return(
         <div className = "footer">
-           <div className = 'footerWrapper'><p>Contact</p><h3>pmichalec.dev@gmail.com</h3></div>
+           <div className = 'footerWrapper'><h2>Contact:  pmichalec.dev@gmail.com</h2></div>
         </div>
     )
 }
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import '../css/Technologies.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {faGithubSquare} from '@fortawesome/free-brands-svg-icons'
+import {faReact} from '@fortawesome/free-brands-svg-icons';
+import {faJsSquare} from '@fortawesome/free-brands-svg-icons';
+import {faHtml5} from '@fortawesome/free-brands-svg-icons';
+import {faCss3Alt} from '@fortawesome/free-brands-svg-icons';
+import {faBootstrap} from '@fortawesome/free-brands-svg-icons';
+import {faNodeJs} from '@fortawesome/free-brands-svg-icons';
+import {faJava} from '@fortawesome/free-brands-svg-icons';
+
 const Technologies = () =>{
     return(
         <div className = "technologies">
             <h1>Technologies</h1>
+            <FontAwesomeIcon icon={faReact} size='7x'/>
+            <FontAwesomeIcon icon={faGithubSquare} size='7x'/>
+            <FontAwesomeIcon icon={faJsSquare} size='7x'/>
+            <FontAwesomeIcon icon={faHtml5} size='7x'/>
+            <FontAwesomeIcon icon={faCss3Alt} size='7x'/>
+            <FontAwesomeIcon icon={faBootstrap} size='7x'/>
+            <FontAwesomeIcon icon={faNodeJs} size='7x'/>
         </div>
     )
 }
